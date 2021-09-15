cc6aa05c61d9b20695526c87fd211577646d9b02
Joseph Lord <git@jtl.me.uk>
2021-09-15 17:17:33 +0100

    Initial Commit

---
+++ UKCovidData/UKCovidDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  UKCovidDataApp.swift
+//  UKCovidData
+//
+//  Created by Joseph Lord on 15/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct UKCovidDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
